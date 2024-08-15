29d86b840d4bc3173a19cda168eb8ba53983f7a9
orbitatecnologiafsa <orbitatecnologiafsa@gmail.com>
2024-08-15 14:37:43 -0300

    Dashboard R3 primeiro commit 15/08

--- server/firebase.js
+++ server/firebase.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 // Substitua pelo caminho para o seu arquivo de chave privada do Firebase
-const serviceAccount = require('./prototiposincronizador-firebase-adminsdk-dtshh-58f592890f.json');
+const serviceAccount = require('/Users/orbita/Desktop/dashboard-AllenCond-main/prototiposincronizador-firebase-adminsdk-dtshh-58f592890f.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
